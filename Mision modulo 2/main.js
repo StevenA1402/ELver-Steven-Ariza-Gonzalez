d84490105e156a8c3b69e166779254b45ef49f72
StevenA1402 <gonzalezelvert@gmail.com>
2023-03-22 13:58:05 -0500

    MIsion

--- Mision modulo 2/main.js
+++ Mision modulo 2/main.js
@@ -1,70 +1,27 @@
-let nombre = document.getElementById('nombre')
-let apellido = document.getElementById('apellido')
-let area = document.getElementById('area')
-let usuario = document.getElementById('usuario')
-let edad = document.getElementById('edad')
-let direccion = document.getElementById('direccion')
-let email = document.getElementById('email')
 let enviar = document.getElementById('enviar')
-let formulario = document.getElementById('formulario')
-let personas = []
-let errores = []
-
-class Formulario {
-    nombre;
-    apellido;
-    area;
-    usuario;
-    edad;
-    direccion;
-    email;
-
-    constructor(nombre,apellido,area,usuario,edad,direccion,email,enviar){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.area = area;
-        this.usuario = usuario;
-        this.edad = edad;
-        this.direccion = direccion;
-        this.email = email 
-    
-    }
-}
-
-
-
-enviar.addEventListener("click" , () => {
-    const persona = new Formulario(
-        nombre.value,
-        apellido.value,
-        area.value,
-        usuario.value,
-        edad.value,
-        direccion.value,
-        email.value
-    );
-    if(nombre.value != "", apellido.value != "", area.value != "", usuario.value != "", edad.value != "", direccion.value != "", direccion.value != "", email.value != ""){
-        errores.push(persona)
-        location.href = 'usuario.html'
-
-    }else{
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Ingresa datos en todos los campos',
-
-          })  }
-
-    setTimeout(function() {
-        localStorage.setItem('formulario', JSON.stringify(personas));
-      }, 100);
-      
-      
-
-    localStorage.setItem('formulario', JSON.stringify(personas))
-    personas.push(persona)
-    console.log(personas);
-
+enviar.addEventListener('click', () => {
+    location.href = 'usuario.html'
 
 })
-    
+
+lista = document.getElementById('lista')
+function info() {
+    const formulario = JSON.parse(localStorage.getItem("formulario"));
+    if (!formulario) {
+      localStorage.setItem(
+        "formulario",
+        JSON.stringify({
+
+        })
+      );
+    }
+    console.log(formulario);
+    lista.innerHTML += `${formulario}`
+    window.addEventListener("load", info);
+
+  }
+
+  window.addEventListener("load", info);
+
+  
+
